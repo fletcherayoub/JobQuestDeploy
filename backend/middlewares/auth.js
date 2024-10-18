3da5b9fee4b7db5c6ffda5f90f5a0283edc2a995
fletcherayoub <douch202@gmail.com>
2024-10-18 20:35:39 +0200

    fictoken

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,25 @@
 import { catchAsyncError } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
-import {User} from "../models/userSchema.js"
+import { User } from "../models/userSchema.js";
+
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-    const { token } = req.cookies;
+    // Check for the token in cookies or Authorization header
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
     if (!token) {
-        return next(new ErrorHandler("not authorized", 400));
+        return next(new ErrorHandler("Not authorized. Token missing.", 401));
     }
+
+    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
+    // Attach the user to the request object for further use
     req.user = await User.findById(decoded.id);
 
-    next();
+    if (!req.user) {
+        return next(new ErrorHandler("User not found.", 404));
+    }
 
-    
-    
+    next();
 });
